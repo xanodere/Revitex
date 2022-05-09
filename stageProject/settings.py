a826f3e901f119d119221769dfd367776cd61c0d
Haddou Younes <91490609+xanodere@users.noreply.github.com>
2022-05-09 17:58:03 +0100

    Update settings.py

--- stageProject/settings.py
+++ stageProject/settings.py
@@ -166,7 +166,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'haddouyounespro@gmail.com'
-EMAIL_HOST_PASSWORD = 'Moujahid00'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
